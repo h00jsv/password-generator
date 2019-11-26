0201aee960b1a8f2b94bc5118787092321783c01
h00jsv <svedin.joakim@gmail.com>
2019-11-25 18:34:42 -0800

    updates

--- main.js
+++ main.js
@@ -14,7 +14,22 @@ const randomFunc = {
     number: getRandomNumber,
     symbol: getRandomSymbol
 };
+// Copy pwd to clipboard
+clipboardEl.addEventListener('click', () => {
+    const textarea = document.createElement('textarea');
+    const password = resultEl.innerText;
 
+    if (!password) {
+        return;
+    }
+
+    textarea.value = password;
+    document.body.appendChild(textarea);
+    textarea.select();
+    document.execCommand('copy');
+    textarea.remove();
+    alert('Password copied to clipboard!');
+})
 
 // Generate eventListener
 generateEl.addEventListener('click', () => {
